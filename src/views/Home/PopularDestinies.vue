b69715c29cd17752da86b051db25e05a02d48a0a
Iván Valenzuela <Ivalenzuela@ntic.cl>
2020-05-07 15:17:00 -0400

    Banners nuevos

--- src/views/Home/PopularDestinies.vue
+++ src/views/Home/PopularDestinies.vue
@@ -4,7 +4,7 @@
     subTitle="Infórmate sobre nuestros servicios, convenios y otros"
     :itemTitles="['Viaja en Bus, la manera más inteligente de viajar', 'Obtén aquí tu Pullman Pass y accede a un mundo de descuentos', 'Conoce aquí los convenios que Pullman Bus tiene para tí']"
     :links="['https://www.viajaenbus.cl/','https://www.pullmanbus.cl/portalWAR/paginas/contenido/pass.jsf','https://www.pullmanbus.cl/portalWAR/paginas/contenido/convenio.jsf#header']"
-    :images="['Imagen-viajaenbus.png', 'bus.png','Imagen-convenios.png']"
+    :images="['Banner2.png', 'Banner3.png','Banner1.png']"
   />
 </template>
 <script>
