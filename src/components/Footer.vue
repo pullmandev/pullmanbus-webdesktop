7c4b5ae6efc1055d59f42d29fe8b499493c95e78
Ivan Valenzuela <ivalenzuela@ntic.cl>
2020-05-08 04:20:11 -0400

    Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
         <v-col cols="3">
           <v-row>
             <v-col cols="12">
-              <span class="body-1 d-block">Llámanos</span>
+              <span class="body-1 d-block">Call Center</span>
               <p class="body-2">
                 <v-icon dark>mdi-phone</v-icon> +58 9 1234 5678
               </p>
@@ -15,12 +15,20 @@
               <div class="d-flex align-center">
                 <div>
                   <v-img
-                    src="../../static/logos/Iconos-33.png"
-                    height="30"
-                    width="30"
+                          src="../../static/logos/Iconos-33.png"
+                          height="30"
+                          width="30"
                   />
                 </div>
                 <span class="body-2">info@pullmanbus.cl</span>
+              </div>
+            </v-col>
+            <v-col cols="12">
+              <span class="body-1 d-block">Casa Matriz Pullman Bus</span>
+              <div class="d-flex align-center">
+                <div>
+                </div>
+                <span class="body-2">San Borja 235, Estación Central, Santiago</span>
               </div>
             </v-col>
           </v-row>
@@ -29,13 +37,18 @@
           <v-row column>
             <v-col cols="12">
               <span class="body-1 d-block">
-                Menú
+                Información al consumidor
+              </span>
+              <span class="body-2 d-block">
+                Conoce tus derechos
                 <v-icon dark>mdi-chevron-right</v-icon>
               </span>
-            </v-col>
-            <v-col cols="12">
-              <span class="body-1 d-block">
-                Información al consumidor
+              <span class="body-2 d-block">
+                Política de privacidad
+                <v-icon dark>mdi-chevron-right</v-icon>
+              </span>
+              <span class="body-2 d-block">
+                Tñerminos y condiciones de pasajes
                 <v-icon dark>mdi-chevron-right</v-icon>
               </span>
             </v-col>
@@ -52,12 +65,14 @@
                 contain
               />
 
-              <span class="float-right">
+              <span class="body-1 d-block text-center">
               Búscanos en
-            <v-icon dark class="ml-1">mdi-facebook</v-icon>
-            <v-icon dark class="ml-1">mdi-instagram</v-icon>
-            <v-icon dark class="ml-1">mdi-youtube</v-icon>
-          </span>
+              </span>
+              <span class="body-1 d-block text-center">
+                <v-icon dark class="ml-1">mdi-facebook</v-icon>
+                <v-icon dark class="ml-1">mdi-instagram</v-icon>
+                <v-icon dark class="ml-1">mdi-youtube</v-icon>
+              </span>
             </v-col>
           </v-row>
         </v-col>
@@ -67,6 +82,10 @@
       <div class="orange_dark px-5">
         <v-card-text class="white--text">
           Todos los derechos reservados, {{ new Date().getFullYear() }}
+          <span class="float-right">
+              Desarrollada por WIT Innovación y Tecnología SPA
+              </span>
+
         </v-card-text>
       </div>
     </v-card>
