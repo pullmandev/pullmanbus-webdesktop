908ffe01fd34505e2746e41734eb326f29c7e2b5
Romulo <romulocg25@gmail.com>
2020-05-28 16:55:32 -0400

    Add fallback when enpont stage yield empty array

--- src/helpers/createBanners.js
+++ src/helpers/createBanners.js
@@ -1,6 +1,22 @@
 import APIBanners from '@/services/api/banners'
 import _ from 'lodash'
 
+function createDefaultObject(type) {
+  let result = []
+  if (type === 3 || type === 4) {
+    result.push({
+      contenido: 'BOLETO DE REGRESO POR ${1}'
+    })
+  } else if (type === 2) {
+    result.push({
+      colorTarifa: '#ffffff',
+      contenido: 'AGREGA TU BOLETO DE REGRESO POR ${1}, QUIERES AGREGARLO?',
+      fondoTarifa: '#FF621D',
+      urlImagen: 'http://pullman.cl/imagenes/Caluga_etapa_2.jpg'
+    })
+  }
+  return result
+}
 function createBanners(searching, type, commit) {
   const { from_city, to_city, from_date } = searching
   if (from_city != null && to_city != null && from_date != null) {
@@ -27,6 +43,9 @@ function createBanners(searching, type, commit) {
           ])
           banners.push(params)
         })
+        if (banners.length <= 0) {
+          banners = createDefaultObject(type)
+        }
         let commitTo = ''
         if (type === 1) {
           commitTo = 'SET_HOME_BANNERS'
