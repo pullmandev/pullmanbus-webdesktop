efbbe8aedbd973b4c3b8ae4417ffc9962c1c8384
Romulo <romulocg25@gmail.com>
2020-06-12 16:59:50 -0400

    Add facebook plugin

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -2,8 +2,30 @@
   <div class="home" id="idHome">
     <SearchPanel />
     <PopularDestinies />
-    <Banner class="my-12" />
+    <Banner class="my-6" />
     <RecentArticles />
+    <v-row>
+      <v-col cols="6" class="d-flex justify-center">
+        <div
+          class="fb-page my-12"
+          data-href="https://www.facebook.com/Pullman.cl/"
+          data-tabs="timeline"
+          data-width=""
+          data-height=""
+          data-small-header="false"
+          data-adapt-container-width="true"
+          data-hide-cover="false"
+          data-show-facepile="true"
+        >
+          <blockquote
+            cite="https://www.facebook.com/Pullman.cl/"
+            class="fb-xfbml-parse-ignore"
+          >
+            <a href="https://www.facebook.com/Pullman.cl/">Pullman Bus</a>
+          </blockquote>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -38,3 +60,8 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.fb-page {
+  width: 300px;
+}
+</style>
