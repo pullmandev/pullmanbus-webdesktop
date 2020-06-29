e32c14b29d25bc856ea8f9c7422964f37489eee1
Romulo <romulocg25@gmail.com>
2020-06-29 18:33:50 -0400

    Revert "Rezising image"
    This reverts commit 7c4542f0a2d434cb95d1ebcb06a32cb882dec650.

--- src/components/Header/Index.vue
+++ src/components/Header/Index.vue
@@ -5,8 +5,8 @@
       <v-btn text @click="$router.push({ path: '/' })" style="height: 100%">
         <v-img
           src="../../../static/logos/pullmanbus_blanco.png"
-          height="60"
-          width="206"
+          height="70"
+          width="240"
         />
       </v-btn>
       <v-spacer></v-spacer>
