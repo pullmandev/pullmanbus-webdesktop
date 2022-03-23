4d15283fa5a91a5663c31e74118b3f99cbd70bc8
Pullman Desarrollo <89167119+pullmandev@users.noreply.github.com>
2022-03-23 11:44:50 -0300

    Update endPoints.js

--- src/endPoints.js
+++ src/endPoints.js
@@ -1,6 +1,6 @@
 //const base = 'https://pullman.cl' // prod
 //const base = 'https://pullmanapi.viajaenbus.cl/' // dev
-const base = 'http://qa.pullman.cl'
+const base = 'https://qa.pullman.cl'
 //const base = ''
 
 const baseVenta = `${base}/integrador-web/rest/private/venta`
